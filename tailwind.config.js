67ed292f2d2c6ff7e1e38051068209856daef5f1
kingrayhan <rayhan095@gmail.com>
2021-11-01 20:13:54 +0600

    footer

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    mode: "jit",
+    prefix: "dope-",
+    purge: ["./**/*.hbs"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
